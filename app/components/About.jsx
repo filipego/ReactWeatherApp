e730d8f4507db31a7340f9ce1016d36bb5ac0f44
Filipe Valente Gomes <Filipe@Filipes-iMac.local>
2017-07-27 17:55:44 -0400

    aded styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var About = React.createClass({
   render: function(){
     return(
       <div>
-        <h3>About</h3>
+        <h1 className="text-center">About</h1>
         <p>Welcome to about page!</p>
       </div>
     );
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,10 +3,10 @@ var React = require('react');
 var WeatherMessage = React.createClass({
   render: function(){
     var {temp, location} = this.props;
-    
+
     return(
       <div>
-        <h3>It is {temp} in {location}.</h3>
+        <h3 className="text-center">It is {temp} in {location}.</h3>
       </div>
     );
   }
