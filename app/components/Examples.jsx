b81f080f7b8a5cdfa3224b035162ec2181dda5c4
Filipe Valente Gomes <Filipe@Filipes-iMac.fios-router.home>
2017-07-26 20:23:59 -0400

    added some foundation styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
+var {Link} =require('react-router');
 
 var Examples = React.createClass({
   render: function(){
     return(
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to examples page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few exomaple locatons to try out:</p>
+        <ol>
+          <li>
+            <Link to="/$location=New York">New York, NY</Link>
+          </li>
+          <li>
+            <Link to="/$location=Rio">Rio, Brazil</Link>
+          </li>
+        </ol>
       </div>
     );
   }
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,13 +3,39 @@ var {Link, IndexLink} = require('react-router');
 
 
 var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    console.log("not yet wired up");
+  },
   render: function(){
     return(
-      <div>
-        <h2>Nav</h2>
-        <IndexLink to="/" activeClassName="actice" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="actice" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="actice" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+          <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="actice" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="actice" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="actice" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+          <ul className="menu">
+            <li>
+              <input type="search" placeholder="Search weather"/>
+            </li>
+            <li>
+              <input type="submit" className="button" value="Get Weather"/>
+            </li>
+          </ul>
+          </form>
+        </div>
       </div>
     );
   }
